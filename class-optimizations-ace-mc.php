b4ea1ea103fb0b8ea485ce14ec871a26e7cff63f
Peter Downey <peterdowney@visistruct.com>
2025-08-28 01:48:18 -0400

    Fixes

--- class-optimizations-ace-mc.php
+++ class-optimizations-ace-mc.php
@@ -285,7 +285,7 @@ class Optimizations_Ace_Mc {
 	 * @return string
 	 */
 	public function display_user_registration_date_column( $output, $column_name, $user_id ) {
-		// Security check - only for admins with list_users capability
+		// Security check - only for admins with list_users capability.
 		if ( ! is_admin() || ! current_user_can( 'list_users' ) ) {
 			return $output;
 		}
